2a92070f8f39e24bbf3fde7180aac18a694e2bd9
tamaaaa2020 <153005305+tamaaaa2020@users.noreply.github.com>
2025-01-22 17:23:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tugas_pbo_api
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tugas_pbo_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas_pbo_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
